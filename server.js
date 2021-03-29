41e4b37c682c308b55d62dae98e3e519fcbcbbd3
pbgnz <pabgonza1995@gmail.com>
2021-03-29 16:35:19 -0400

    #2 implementation of identity map

--- server.js
+++ server.js
@@ -9,25 +9,34 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(router);
 
+let identityMap = new Map();
+
 router.get('/api/quotes', (req, res) => {
     let pageNumber = Math.floor(Math.random() * 100) + 1;
-    let quotes = [];
-    request('https://www.goodreads.com/quotes?page=' + pageNumber, function (err, r, body) {
-        if (err) {
-            res.send("401: Error getting quotes");
-        } else {
-            let $ = cheerio.load(body);
-            $('div.quoteText').each(function (index) {
-                const quote = $(this)[0].children[0].data.replace(/(\r\n|\n|\r| +(?= )| “|”)/gm, "");
-                const id = index;
-                quotes.push({ quote, id });
-            });
-            $('span.authorOrTitle').each(function (index) {
-                quotes[index].author = $(this)[0].children[0].data.replace(/(\r\n|\n|\r| +(?= )|,)/gm, "");
-            });
-            res.send({ quotes });
-        }
-    });
+
+    if (identityMap.has(pageNumber)) {
+        const quotes = identityMap.get(pageNumber);
+        res.send({ quotes });
+    } else {
+        let quotes = [];
+        request('https://www.goodreads.com/quotes?page=' + pageNumber, function (err, r, body) {
+            if (err) {
+                res.send("401: Error getting quotes");
+            } else {
+                let $ = cheerio.load(body);
+                $('div.quoteText').each(function (index) {
+                    const quote = $(this)[0].children[0].data.replace(/(\r\n|\n|\r| +(?= )| “|”)/gm, "");
+                    const id = index;
+                    quotes.push({ quote, id });
+                });
+                $('span.authorOrTitle').each(function (index) {
+                    quotes[index].author = $(this)[0].children[0].data.replace(/(\r\n|\n|\r| +(?= )|,)/gm, "");
+                });
+                identityMap.set(pageNumber, quotes);
+                res.send({ quotes });
+            }
+        });
+    }
 });
 
 app.listen(port);
