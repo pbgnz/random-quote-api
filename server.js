02c1aa8c7b00657bb7fdecf5a510502df8795102
pbgnz <pbgnz@users.noreply.github.com>
2024-04-15 21:45:49 -0400

    #4 configuring dynamic CORS Origins

--- server.js
+++ server.js
@@ -13,8 +13,15 @@ const limiter = rateLimit({
   max: 1000
 });
 
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:8080', 'https://escobot.github.io']; // add frontend application
 const corsOptions = {
-    origin: 'https://escobot.github.io',
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     optionsSuccessStatus: 200,
     methods: "GET"
 }
